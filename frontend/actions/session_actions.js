03d96accd22aed7cd31659b7b39397cc9f879fef
Abigail <absterr08@berkeley.edu>
2018-01-02 14:42:51 -0500

    added reducers and store

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,1 +1,40 @@
-import * as SessionApiUtil from '../util/session_api_util';
+import * as SessionAPIUtil from '../util/session_api_util.js';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = (user) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user
+  };
+};
+
+export const receiveSessionErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
+export const login = (user) => (dispatch) => {
+	return SessionAPIUtil.login(user).then((user) =>
+    dispatch(receiveCurrentUser(user)),
+    (err) => dispatch(receiveSessionErrors(err.responseJSON))
+  );
+};
+
+export const logout = () => (dispatch) => {
+  return SessionAPIUtil.logout().then(() =>
+    dispatch(receiveCurrentUser(null)),
+    (err) => dispatch(receiveSessionErrors(err.responseJSON))
+  );
+};
+
+export const signup = (user) => (dispatch) => {
+  return SessionAPIUtil.signup(user).then((user) =>
+    dispatch(receiveCurrentUser(user)),
+    (err) => dispatch(receiveSessionErrors(err.responseJSON))
+  );
+};
--- frontend/slap.jsx
+++ frontend/slap.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'
+//testing
+import * as SessionAPIUtil from './actions/session_actions';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<h1>Welcome to Slap</h1>, root);
+
+  //testing
+  window.signup = SessionAPIUtil.signup;
+  window.logout = SessionAPIUtil.logout;
+  window.login = SessionAPIUtil.login;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
