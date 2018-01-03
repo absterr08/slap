211648f73bb104287ce9970e501c684e88fea87a
Abigail <absterr08@berkeley.edu>
2018-01-03 00:31:19 -0500

    Splash styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,11 +3,12 @@ import {Route} from 'react-router-dom';
 import SessionFormContainer from './session_form/session_form_container';
 import { AuthRoute } from '../util/route_util';
 import GreetingContainer from './greeting';
+import Splash from './splash/splash';
 
 
 const App = () => (
   <div>
-    <GreetingContainer />
+    <Splash />
     <AuthRoute path="/login" component={SessionFormContainer} />
     <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
---
+++ frontend/components/navbar/navbar.jsx
@@ -0,0 +1,39 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import { logout } from '../actions/session_actions';
+
+
+const Navbar = ({ username, logout }) => {
+  if (!username) {
+    return (
+      <div>
+        <Link to="/signup" >Sign Up!</Link>
+        <br/>
+        <Link to="/login" >Log In!</Link>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Hi, {username}</h1>
+        <button onClick={logout}>Log Out</button>
+      </div>
+    )
+  }
+};
+
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    username: state.session.currentUser ? state.session.currentUser.username : ""
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch(logout())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
