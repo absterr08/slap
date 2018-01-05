3239e09db5e01d0d0a129c1d109457915e7ab5b2
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-05 13:29:37 -0500

    configure redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,12 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  # set production cable server URI
+  config.web_socket_server_url = "wss://aa-slap.herokuapp.com/cable"
+
+  # allow request origins
+  config.action_cable.allowed_request_origins = ['https://aa-slap.herokuapp.com', 'http://aa-slap.herokuapp.com']
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
