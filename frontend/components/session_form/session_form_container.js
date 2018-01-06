7d04c36ab17937b393f35a06a3a1527f4bbf2006
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-06 17:02:01 -0500

    add guest login

--- frontend/components/session_form/session_form_container.js
+++ frontend/components/session_form/session_form_container.js
@@ -12,7 +12,7 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  const action = ownProps.match.path === "/signup" ? signup : login
+  const action = ownProps.match.path === "/signup" ? signup : login;
   return {
     processForm: user => dispatch(action(user))
   };
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import SplashContainer from './splash_container';
+import { login } from '../../actions/session_actions'
 
 
-const Splash = () => (
+const Splash = ({ demoLogin }) => (
   <div className="splash-container">
     <div className="splash-image"/>
     <div className="main-info">
       <h1>Productivity starts here</h1>
       <p>Whip your team into shape with Slap! A messaging app for teams that
         need a litle extra push in the right direction.</p>
-      <Link to="/signup" className="demo-btn">Get started</Link>
+      <div className="signup-button-container">
+        <Link to="/signup" className="signup-btn">Get started</Link>
+        <button onClick={demoLogin} className="signup-btn">Guest Login</button>
+      </div>
 
       <p>Already using Slap?&nbsp;
       <Link to="/login" className="splash-link">Sign In</Link>
@@ -19,4 +23,11 @@ const Splash = () => (
   </div>
 );
 
-export default Splash;
+
+const mapDispatchToProps = dispatch => (
+  {
+    demoLogin: () => dispatch(login({ username: 'guestUser', password: 'starwars' }))
+  }
+)
+
+export default connect(null, mapDispatchToProps)(Splash);
