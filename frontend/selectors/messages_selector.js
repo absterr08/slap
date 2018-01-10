75a3bab041efeb2c9681d86630aa67c52f0b3c1c
Abigail <absterr08@berkeley.edu>
2018-01-10 10:28:09 -0500

    remove multiple message rendering before a refresh

--- frontend/selectors/messages_selector.js
+++ frontend/selectors/messages_selector.js
@@ -1,7 +1,9 @@
+import { values } from 'lodash';
+
 export const selectCurrentChannelMessages = state => {
-  debugger
+  // debugger
   const allMessages = state.entities.messages;
-  const channelMessageIds = state.ui.currentChannel.messages;
+  const channelMessageIds = values(state.ui.currentChannel.messages);
   const filteredMessages = Object.assign({}, allMessages);
   channelMessageIds.map( id => delete filteredMessages[id]);
   return filteredMessages;
