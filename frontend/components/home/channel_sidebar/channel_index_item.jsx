5f3a9e2c37695da0c25a0d43485248b055cd86ef
Abigail <absterr08@berkeley.edu>
2018-01-11 11:07:41 -0500

    add styling

--- frontend/components/home/channel_sidebar/channel_index_item.jsx
+++ frontend/components/home/channel_sidebar/channel_index_item.jsx
@@ -6,18 +6,19 @@ import { selectDmNames } from '../../../util/channel_api_util';
 
 const ChannelIndexItem = ({ channel, currentUsername }) => {
   const channelInfo = channel.channel;
-  let title;
+  let title, iconType;
   if (currentUsername) {
     title = selectDmNames(channel, currentUsername).join(', ')
-
+    iconType = "dm-list-item-icon"
   } else {
     title = channelInfo.name;
+    iconType = "channel-list-item-icon"
   }
   return (
       <Link to={ `/messages/${channelInfo.id}` }>
         <li className="channel-list-item">
           <div className="channel-list-item-container">
-            <div className="channel-list-item-logo"></div>
+            <div className={iconType}></div>
             <div className="chennel-list-item-name">{title}</div>
           </div>
         </li>
