7ddb22149de477e21e1a3762d99cf24bf27618de
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-12 10:18:11 -0500

    style messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,13 @@ u4 = User.create!(username: "leslieKnope", email: "d", password: "starwars", img
 u5 = User.create!(username: "bman68", email: "e", password: "starwars", img_url: "3")
 u6 = User.create!(username: "sunnyBunny", email: "f", password: "starwars", img_url: "8")
 u7 = User.create!(username: "mauricio12", email: "g", password: "starwars", img_url: "9")
+u8 = User.create!(username: "monmon", email: "h", password: "starwars", img_url: "10")
+u8 = User.create!(username: "daedae", email: "i", password: "starwars", img_url: "11")
 
 c1 = Channel.create!(name: '11-06-2017-nyc', description: 'Welcome to Slap Academy!')
 c2 = Channel.create!(name: 'general', description: 'This channel is for team-wide communication and announcements.')
 c3 = Channel.create!(name: 'random', description: 'A place for non-work-related slap chats.')
-c4 = Channel.create!(name: 'slap-talk', description: 'Hand in your best hand-related puns' )
+c4 = Channel.create!(name: 'slap-talk', description: 'Hand in your best slap-related puns' )
 # c5 = Channel.create!(name: '', description: )
 # c6 = Channel.create!(name:, description: )
 
@@ -37,17 +39,22 @@ Message.create!(author_id: u2.id, channel_id: c1.id, body: "lol never heard of i
 Message.create!(author_id: u3.id, channel_id: c1.id, body: "ya what package is that?")
 
 #channel2 messages (general)
-Message.create!(author_id: u5.id, channel_id: c2.id, body: "what is the sound of one hand clapping")
+Message.create!(author_id: u5.id, channel_id: c3.id, body: "what is the sound of one hand clapping")
 
 #channel 3 messages (random)
 Message.create!(author_id: u2.id, channel_id: c2.id, body: "party at my place tonight y'all")
 Message.create!(author_id: u6.id, channel_id: c2.id, body: "ayyyyy i'll be there!")
 
+#channel 4 messages (slap talk)
+Message.create!(author_id: u5.id, channel_id: c4.id, body: "what did the hand say to the face? SLAP!")
+Message.create!(author_id: u7.id, channel_id: c4.id, body: "lol good one, high five!")
+Message.create!(author_id: u1.id, channel_id: c4.id, body: "Redux was really difficult to grasp at first, but i think we all have a great handle on it now!")
 
 
 dm1 = Channel.new(name:"dm1", is_dm: true)
 dm1.users = [u1, u6, u7]
 dm1.save!
 #
-# dm2 = Channel.new(name:"dm2")
+dm2 = Channel.new(name:"dm2")
+dm2.users = [u1, u8, u9]
 # dm3 = Channel.new(name:"dm3")
