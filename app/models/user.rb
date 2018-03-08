0dd22520917146a787ce0c6b072b3e8c4bf88991
abby <abbychersh@gmail.com>
2018-03-08 11:44:55 -0500

    integrate paperclip and aws

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
   has_many :channels,
     through: :channel_subscriptions
 
+  has_attached_file :avatar, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   attr_reader :password
 
 
